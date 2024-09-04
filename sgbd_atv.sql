78dfb98fe1429bd589dfeeccfe5225a051b44c72
gv7 <145298212+gv137@users.noreply.github.com>
2024-09-04 09:37:57 -0300

    sgbd estudos
    aprendendo sobre DQL dentro do MySQL

---
+++ sgbd_atv.sql
@@ -0,0 +1,47 @@
+create database industria;
+
+use industria;
+
+create table pessoas(
+codigo integer,
+nome varchar(100),
+cpf int(14),
+rg int(12),
+email varchar(100)
+);
+
+select*from funcionarios;
+
+alter table pessoas rename to funcionarios;
+
+alter table funcionarios change telefone celular int(10);
+
+alter table funcionarios add data_nascimento date;
+
+alter table funcionarios modify column celular int(10) not null; 
+
+describe funcionarios;
+
+alter table funcionarios drop rg;
+
+insert into funcionarios(codigo, nome, cpf) value('2000-05-13');
+
+create table pagamentos(
+cod_boleto integer primary key,
+valor float
+);
+
+select*from pagamentos;
+describe pagamentos;
+
+insert into pagamentos(cod_boleto, valor) value(1, 250.00), (2, 120.00), (3, 450.00), (4, 980.00), (5, 120.00), (6, 300.00), (7, 50.00), (8, 500.00);
+insert into pagamentos(cod_boleto, valor) value(9, 250.50);
+
+select*from pagamentos where cod_boleto > 3;
+select*from pagamentos where not cod_boleto = 5;
+select*from pagamentos where cod_boleto  between  3 and  8;
+select*from pagamentos where cod_boleto > 3;
+
+select avg(valor) from pagamentos;
+select min(valor) from pagamentos;
+select max(valor) from pagamentos;
